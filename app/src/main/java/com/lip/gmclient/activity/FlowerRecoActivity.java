c0f8da06b5b50da96798d42529338afb6ade8c43
LoveAlwaysYoung <729950490@qq.com>
2018-04-26 20:01:40 +0800

    完成Yukino识花接口

--- app/src/main/java/com/lip/gmclient/activity/FlowerRecoActivity.java
+++ app/src/main/java/com/lip/gmclient/activity/FlowerRecoActivity.java
@@ -8,14 +8,27 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.lip.gmclient.R;
+import com.lip.gmclient.domain.TaskBean;
+import com.lip.gmclient.utils.Constant;
+import com.lip.gmclient.utils.Md5Util;
 import com.lip.gmclient.utils.TakePictureManager;
+import com.lip.gmclient.utils.TencentAIUtil;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
 
 import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FlowerRecoActivity extends AppCompatActivity {
 
@@ -31,7 +44,36 @@ public class FlowerRecoActivity extends AppCompatActivity {
         takePictureManager.setTakePictureCallBackListener(new TakePictureManager.takePictureCallBackListener() {
             @Override
             public void successful(boolean isTailor, File outFile, Uri filePath) {
+                TreeMap<String,String> map=new TreeMap<>();
+                String image= TencentAIUtil.fileToBase64(outFile);
+                String time_stamp = System.currentTimeMillis() / 1000 + "";
+                String nonce_str=TencentAIUtil.getRandomString(16);
+                String sign=null;
+                map.put("image",image);
+                map.put("app_id",Constant.Tencent_AppID);
+                map.put("time_stamp",time_stamp);
+                map.put("scene","2");
+                map.put("nonce_str",nonce_str);
+                try{
+                     sign=TencentAIUtil.generateAppSign(map);
+                     map.put("sign",sign);
+                }catch (Exception e){
+                    Toast.makeText(context,"生成密钥失败",Toast.LENGTH_SHORT).show();
+                }
+                // 网络请求
+                OkGo.<String>post(Constant.Tencent_API)
+                        .params(map)
+                        .execute(new StringCallback() {
+                            @Override
+                            public void onSuccess(Response<String> response) {
 
+                            }
+
+                            @Override
+                            public void onError(Response<String> response) {
+
+                            }
+                        });
             }
 
             @Override
--- app/src/main/java/com/lip/gmclient/utils/TencentAIUtil.java
+++ app/src/main/java/com/lip/gmclient/utils/TencentAIUtil.java
@@ -1,52 +1,154 @@
 package com.lip.gmclient.utils;
 
 import android.util.Base64;
+import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class TencentAIUtil {
 
-    /*
-    接口鉴权->计算步骤
-    1.将<key, value>请求参数对按key进行字典升序排序，得到有序的参数对列表N
-    2.将列表N中的参数对按URL键值对的格式拼接成字符串，得到字符串T（如：key1=value1&key2=value2），URL键值拼接过程value部分需要URL编码，URL编码算法用大写字母，例如%E8，而不是小写%e8
-    3.将应用密钥以app_key为键名，组成URL键值拼接到字符串T末尾，得到字符串S（如：key1=value1&key2=value2&app_key=密钥)
-    4.对字符串S进行MD5运算，将得到的MD5值所有字符转换成大写，得到接口请求签名
-     */
-    public static String getReqSign(String image,String nonce_str,int scene,String time_stamp){
-        String T="app_id="+Constant.Tencent_AppID+"&image="+image+"&nonce_str="+nonce_str+"&scene="+scene+"&time_stamp="+time_stamp;
-        String S=T+"&app_key="+Constant.Tencent_AppKey;
-        String MD5T=Md5Util.md5(S);
-        return MD5T.toUpperCase();
+    // 随机字符串
+    public static String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
     }
 
+    // 鉴权
+    public static String generateAppSign(TreeMap<String,String> mParams) throws UnsupportedEncodingException {
+        Set<String> keySet = mParams.keySet();
+        StringBuilder sb = new StringBuilder();
+        Iterator<String> iterator = keySet.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            String value = mParams.get(key);
+            sb.append("&").append(key).append("=").append(URLEncoder.encode(value, "UTF-8"));
+        }
+        sb.deleteCharAt(0);
+        sb.append("&app_key=").append(Constant.Tencent_AppKey);
+        String sign = Md5Util.md5(sb.toString()).toUpperCase();
+        return sign;
+    }
 
     // 将文件转为Base64
     public static String fileToBase64(File file) {
-        String base64 = null;
-        InputStream in = null;
-        try {
-            in = new FileInputStream(file);
-            byte[] bytes = new byte[in.available()];
-            int length = in.read(bytes);
-            base64 = Base64.encodeToString(bytes, 0, length, Base64.DEFAULT);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
+           String res;
+           try {
+               res=encode(readFileByBytes(file));
+           }catch (Exception e){
+               return null;
+           }
+           return res;
+    }
+
+    private static final char last2byte = (char) Integer.parseInt("00000011", 2);
+    private static final char last4byte = (char) Integer.parseInt("00001111", 2);
+    private static final char last6byte = (char) Integer.parseInt("00111111", 2);
+    private static final char lead6byte = (char) Integer.parseInt("11111100", 2);
+    private static final char lead4byte = (char) Integer.parseInt("11110000", 2);
+    private static final char lead2byte = (char) Integer.parseInt("11000000", 2);
+    private static final char[] encodeTable = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+    private static String encode(byte[] from) {
+        StringBuilder to = new StringBuilder((int) ((double) from.length * 1.34D) + 3);
+        int num = 0;
+        char currentByte = 0;
+
+        int i;
+        for (i = 0; i < from.length; ++i) {
+            for (num %= 8; num < 8; num += 6) {
+                switch (num) {
+                    case 0:
+                        currentByte = (char) (from[i] & lead6byte);
+                        currentByte = (char) (currentByte >>> 2);
+                    case 1:
+                    case 3:
+                    case 5:
+                    default:
+                        break;
+                    case 2:
+                        currentByte = (char) (from[i] & last6byte);
+                        break;
+                    case 4:
+                        currentByte = (char) (from[i] & last4byte);
+                        currentByte = (char) (currentByte << 2);
+                        if (i + 1 < from.length) {
+                            currentByte = (char) (currentByte | (from[i + 1] & lead2byte) >>> 6);
+                        }
+                        break;
+                    case 6:
+                        currentByte = (char) (from[i] & last2byte);
+                        currentByte = (char) (currentByte << 4);
+                        if (i + 1 < from.length) {
+                            currentByte = (char) (currentByte | (from[i + 1] & lead4byte) >>> 4);
+                        }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                to.append(encodeTable[currentByte]);
             }
         }
-        return base64;
+        if (to.length() % 4 != 0) {
+            for (i = 4 - to.length() % 4; i > 0; --i) {
+                to.append("=");
+            }
+        }
+        Log.i("lip","网上的base64："+to.toString());
+        return to.toString();
     }
+    /**
+     * 根据文件路径读取byte[] 数组
+     */
+    private static byte[] readFileByBytes(File file) throws IOException {
+        if (!file.exists()) {
+            throw new FileNotFoundException(file.getAbsolutePath());
+        } else {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream((int) file.length());
+            BufferedInputStream in = null;
+
+            try {
+                in = new BufferedInputStream(new FileInputStream(file));
+                short bufSize = 1024;
+                byte[] buffer = new byte[bufSize];
+                int len1;
+                while (-1 != (len1 = in.read(buffer, 0, bufSize))) {
+                    bos.write(buffer, 0, len1);
+                }
+
+                byte[] var7 = bos.toByteArray();
+                return var7;
+            } finally {
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                } catch (IOException var14) {
+                    var14.printStackTrace();
+                }
+
+                bos.close();
+            }
+        }
+    }
+
 }
